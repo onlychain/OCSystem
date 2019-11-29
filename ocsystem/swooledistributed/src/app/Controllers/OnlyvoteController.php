cfaad3234afefcf45b432a209fabef66dbd359d1
652943950@qq.com <652943950@qq.com>
2019-11-29 14:55:24 +0800

    New pledge function and get pledge time development

--- ocsystem/swooledistributed/src/app/Controllers/OnlyvoteController.php
+++ ocsystem/swooledistributed/src/app/Controllers/OnlyvoteController.php
@@ -220,4 +220,149 @@ class OnlyvoteController extends Controller
         return $this->http_output->lists($res['Data']);
     }
 
+
+    /**
+     * 确定质押交易
+     */
+    public function http_getPledgeDeal(){
+        $address = $this->http_input->getAllPostGet();
+        $res = $this->VoteClass->getPledgeDeal($address);
+        if(!$res['IsSuccess']){
+            return $this->http_output->notPut('', $res['Message']);
+        }
+        return $this->http_output->lists($res);
+    }
+
+    /**
+     * 质押列表
+     */
+    public function http_getPledgeList()
+    {
+        $address = $this->http_input->getPost('address');
+        $page = $this->http_input->getPost('currentPage');
+        $pagesize = $this->http_input->getPost('pageSize');
+        $sort = $this->http_input->getPost('sort');
+        $type = $this->http_input->getPost('type');
+        $res = $this->VoteClass->getPledgeList($address,$page,$pagesize,intval($sort),intval($type));
+        if(!$res['IsSuccess']){
+            return $this->http_output->notPut('', $res['Message']);
+        }
+        return $this->http_output->lists($res['Data']);
+    }
+
+    /**
+     * 质押页面
+     */
+    public function http_getPledgeInfo(){
+        $address = $this->http_input->getPost('address');
+        $page = $this->http_input->getPost('currentPage');
+        $pagesize = $this->http_input->getPost('pageSize');
+        $res = $this->VoteClass->getPledgeInfo($address,$page,$pagesize);
+        if(!$res['IsSuccess']){
+            return $this->http_output->notPut('', $res['Message']);
+        }
+        return $this->http_output->lists($res['Data']);
+    }
+
+
+    /**
+     * 获取质押的解锁时间
+     */
+    public function http_pledgeTimes(){
+        $address = $this->http_input->getPost('address');
+        $sort = $this->http_input->getPost('sort');
+        $type = $this->http_input->getPost('type');
+        $page = $this->http_input->getPost('currentPage') ?? 1;
+        $pagesize = $this->http_input->getPost('pageSize');
+        $res = $this->VoteClass->getPledgeTimes($address,$sort,$type,$page,$pagesize);
+        if(!$res['IsSuccess']){
+            return $this->http_output->notPut('', $res['Message']);
+        }
+        return $this->http_output->lists($res['Data']);
+    }
+
+    /**
+     * 转账记录
+     */
+    public function http_transferList()
+    {
+        $address = $this->http_input->getAllPostGet();
+        $page = $this->http_input->getPost('page');
+        $pagesize = $this->http_input->getPost('pagesize');
+        $res = $this->VoteClass->transferList($address,$page ,$pagesize);
+        if(!$res['IsSuccess']){
+            return $this->http_output->notPut('', $res['Message']);
+        }
+        return $this->http_output->lists($res['Data']);
+    }
+
+    /**
+     * 转账详情
+     */
+    public function http_transferDetails()
+    {
+        $trading = $this->http_input->getPost('trading');//trading是查询主键ID
+        $address = $this->http_input->getPost('address');
+        $res = $this->VoteClass->transferDetails($trading, $address);
+        if(!$res['IsSuccess']){
+            return $this->http_output->notPut('', $res['Message']);
+        }
+        return $this->http_output->lists($res['Data']);
+    }
+
+    /**
+     * 投票奖励的列表
+     */
+    public function http_voteReward()
+    {
+        $address = $this->http_input->getPost('address');
+        $page = $this->http_input->getPost('currentPage');
+        $pagesize = $this->http_input->getPost('pageSize');
+        $res = $this->VoteClass->voteReward($address,$page,$pagesize);
+        if(!$res['IsSuccess']){
+            return $this->http_output->notPut('', $res['Message']);
+        }
+        return $this->http_output->lists($res['Data']);
+    }
+
+
+    /**
+     * 投票奖励的列表
+     */
+    public function http_voteRewardInfo()
+    {
+        $address = $this->http_input->getPost('address');
+        $id = $this->http_input->getPost('id');
+        $res = $this->VoteClass->voteRewardInfo($id, $address);
+        if(!$res['IsSuccess']){
+            return $this->http_output->notPut('', $res['Message']);
+        }
+        return $this->http_output->lists($res['Data']);
+    }
+
+
+    /**
+     * 获取货币单位
+     */
+    public function http_monetaryUnit()
+    {
+        $res = $this->VoteClass->monetaryUnit();
+        if(!$res['IsSuccess']){
+            return $this->http_output->notPut('', $res['Message']);
+        }
+        return $this->http_output->lists($res['Data']);
+    }
+
+    /**
+     * 获取网络设置
+     */
+    public function http_networkSettings()
+    {
+        $res = $this->VoteClass->networkSettings();
+        if(!$res['IsSuccess']){
+            return $this->http_output->notPut('', $res['Message']);
+        }
+        return $this->http_output->lists($res['Data']);
+    }
+
 }
